0cba455f89d7d5d6b462e17ce4f6914a46057498
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-07-13 14:33:29 +0300

    Update providers.tsx

--- Providers/providers.tsx
+++ Providers/providers.tsx
@@ -7,7 +7,6 @@ import { Toaster } from "react-hot-toast";
 import { Offline, Online } from "react-detect-offline";
 import { MdOutlineWifiOff } from "react-icons/md";
 import LoadingScreen from "@/components/Layout/LoadingScreen";
-import { VideoInfo } from "@/functions/VideoInfo";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     const [loading, setLoading] = useState(true);
