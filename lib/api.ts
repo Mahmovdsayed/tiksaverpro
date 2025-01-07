a16d91a0d3dfe432736c414c5ea7527d5a978065
Mahmovdsayed <mahmoudsayed3576@gmail.com>
2025-01-07 12:45:41 +0200

    fix-err

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://tiksaverpro.vercel.app",
   method: "POST",
   headers: {
     "Content-Type": "application/json",
